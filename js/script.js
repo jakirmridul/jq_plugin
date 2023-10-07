59cd27a9185acc3f4921f949f1fca9931b133eb0
Jakir Hasan Mridul <jakirhasan.mridul@gmail.com>
2023-10-07 14:55:53 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -74,6 +74,124 @@ AOS.init({
 	easing: 'linear'
 });
 
+ var mixer = mixitup('#mainTab');
+    var mixer = mixitup('#mainTab', {
+       
+        animation: {
+            duration: 300
+        },
+        animation: {
+        easing: 'ease-in-out'
+        }
+    });
+
+  lightbox.option({
+      'resizeDuration': 300,
+      'wrapAround': true
+    });
+
+$('.slider_cont').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 300,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [
+
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }   
+  ]
+
+    });
+$('.demo_cont').slick({
+    dots: true,
+    draggable: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    centerMode: true,
+    centerPadding: '60px',
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+    ]
+
+  });
+
+  $('.demo_cont2').slick({
+    speed: 500,
+  fade: true,
+  cssEase: 'linear',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+
+
+  // Slider Syncing
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  speed: 700,
+  centerMode: true,
+  focusOnSelect: true
+});
 
 
 
